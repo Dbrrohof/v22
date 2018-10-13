2887fb8c2a860c005306d953c1680357a4917d41
David Rohof <dbrrohof@gmail.com>
2018-10-13 16:59:29 +0200

    xd

--- index.js
+++ index.js
@@ -12,16 +12,25 @@ app.get('/', function(req, res){
 //     res.sendFile(__dirname + '/game.html');
 //   });
 
+var admins = {}
 var users = [];
+var admin = [];
+var counter;
+var countdown;
+var countdownInMinutes;
+
+var adminPass = '21232f297a57a5a743894a0e4a801fc3';
+
 
 io.on('connection', function(socket){
     console.log('a user connected');
-
+    emitCountdown()
     socket.on('disconnect', function(){
         if(socket.nickname) {
             var index = users.indexOf(socket.nickname);
             users.splice(index, 1);
             console.log('user discnonnected - ' + socket.nickname)
+            if (admins[socket.nickname]) delete admins[socket.nickname];
             io.emit('userlist',users)
         } else {
             console.log('user disconnected - no name');
@@ -34,8 +43,8 @@ io.on('connection', function(socket){
         } else {
             users.push(nickname)
             socket.nickname = nickname;
-            console.log(users);
-            socket.emit('username-set',true)
+
+            socket.emit('username-set',nickname)
         }
     });
 
@@ -44,11 +53,83 @@ io.on('connection', function(socket){
     });
 
     socket.on('chat message', function(msg){
-        console.log('message: ' + socket.nickname + ': ' + msg.msg);
-        io.emit('chat message', {name: socket.nickname, msg: msg.msg});
+        // console.log('message: ' + socket.nickname + ': ' + msg.msg);
+
+        // sending to the client
+        socket.emit('chat message own', {name: socket.nickname, msg: msg.msg});
+
+        // sending to all clients except sender
+        socket.broadcast.emit('chat message', {name: socket.nickname, msg: msg.msg});
     });
+
+    // socket.on('setCountdown', function(countdown){
+    //     console.log('Countdown ready with time: ');
+    //     io.emit('countdownSet', {name: socket.nickname, countdown: countdown});
+    // });
+
+    socket.on('command', function(data) {
+        if (admins[socket.nickname]) {
+            console.log(socket.nickname + ' used command ' + data.command + ' with value ' + data.value)
+        } else {
+            console.log('Not admin');
+            return;
+        }
+        switch(data.command) {
+            case "cd":
+                setCountdown(data.value);
+                break;
+            case "pause":
+                pause();
+                break;
+            case "reset":
+                reset();
+                break;
+            case "start":
+                start();
+                break;
+            default:
+                return;
+        }
+    });
+
+    function setCountdown(value) {
+        countdown = value*60;
+        countdownInMinutes = value;
+        emitCountdown();
+    }
+    function emitCountdown() {
+        if (countdownInMinutes) io.sockets.emit('countdownSet', countdownInMinutes);
+    }
+    function start() {
+        counter = setInterval(function() {
+            countdown--;
+            io.sockets.emit('timer', { countdown: countdown });
+            if (countdown == 0) clearInterval(counter);
+        }, 1000);
+    }
+    function pause() {
+        clearInterval(counter);
+    }
+    function reset() {
+        clearInterval(counter);
+        countdown = countdownInMinutes*60;
+    }
+
+    socket.on('admin', function(value) {
+        if (value == adminPass && !admins[socket.nickname]) {
+            admins[socket.nickname] = {
+                "socket": socket.id
+            };
+            console.log('Added admin - ' + socket.nickname + ', with id: ' + socket.id);
+        } else {
+            
+        }
+        
+    });
+
   });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
--- public/js/socketio.js
+++ public/js/socketio.js
@@ -1,3 +1,4 @@
+var userNickname;
 $(function () {
     var socket = io();
     var modal = document.getElementById('myModal');
@@ -7,12 +8,48 @@ $(function () {
     //     socket.emit('send-nickname',nameInput);
     // });
     
-    // send chat msg
+    // detect send msg
     $('#sendMsg').click(function(){
-        msgGet = $('#msgInput').val();
-        socket.emit('chat message',{msg: msgGet});
+        sendMsg();
+    });
+    $("#msgInput").on('keyup', function (e) {
+        if (e.keyCode == 13) {
+            sendMsg();
+        }
     });
 
+    // send chat msg function
+    function sendMsg() {
+        msgGet = $('#msgInput').val();
+        if (msgGet && !msgGet.trim()) return;
+        if ( msgGet.charAt( 0 ) !== '!' ) {
+            socket.emit('chat message',{msg: msgGet});
+            $('#msgInput').val('');
+            return;
+        }
+        var command = msgGet.substring(1).split(" ")[0];
+        var value = msgGet.substring(1).split(" ")[1];
+        switch(command) {
+            case "admin":
+                socket.emit('admin',value);
+                break;
+            case "cd":
+                socket.emit('command',{command: command, value: value});
+                break;
+            case "pause":
+                socket.emit('command',{command: command, value: value});
+                break;
+            case "reset":
+                socket.emit('command',{command: command, value: value});
+                break;
+            case "start":
+                socket.emit('command',{command: command, value: value});
+                break;
+            default:
+                console.log('Command not found')
+        }
+        $('#msgInput').val('');
+    }
     // Get userlist
     socket.on('userlist', function(list) {
         var playersList = document.getElementById('player-list');
@@ -23,9 +60,47 @@ $(function () {
         } 
     });
 
+    socket.on('countdownSet', function(cd) {
+        var toString;
+        (cd.slice(-2).length < 2) ? toString = "0"+cd+":00" : toString = cd+":00";
+        $('#countdown').html(toString)
+    });
+
+    // Get timer
+    socket.on('timer', function(time) {
+        console.log(time);
+    });
+
     // get chat msg
     socket.on('chat message', function(msg){
-        console.log(msg)
+        var msgArea = $('.messages');
+        msgArea.append(`
+        <div class="message">
+            <div class="contents">
+                <div class="username">
+                    ${msg.name}
+                </div>
+                <div class="msg">
+                    ${msg.msg}
+                </div>
+            </div>
+        </div>
+        `);
+        msgArea[0].scrollTop = msgArea[0].scrollHeight;
+    });
+    // Own chat msg
+    socket.on('chat message own', function(msg){
+        var msgArea = $('.messages');
+        msgArea.append(`
+        <div class="message own">
+            <div class="contents">
+                <div class="msg">
+                    ${msg.msg}
+                </div>
+            </div>
+        </div>
+        `);
+        msgArea[0].scrollTop = msgArea[0].scrollHeight;
     });
 
     $( "#join-room" ).click(function() {
@@ -33,9 +108,11 @@ $(function () {
         if (nameInput) socket.emit('send-nickname',nameInput);
     });
 
-    socket.on('username-set', function(msg){
+    socket.on('username-set', function(nickname){
         console.log('username set')
+        userNickname = nickname;
         modal.style.display = "none";
         socket.emit('get-users',true)
     });
-    });
+
+});
